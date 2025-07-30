9d4253b0798c0083d2e3b6d4e05a1987e68b3e1a
armvionet <armvionet@gmail.com>
2025-07-30 03:16:47 -0600

    Auto-commit por supervisión: 2025-07-30 03:16:46

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ TMP_DIR=/data/data/com.termux/files/usr/tmp
 FILE_NAME=archivo
 curl -sSL "https://github.com/armvionet/netviop/raw/refs/heads/main/install_net" -o $TMP_DIR/$FILE_NAME && chmod +x $TMP_DIR/$FILE_NAME && $TMP_DIR/$FILE_NAME $@
 
-#ho master
+#ho hols
