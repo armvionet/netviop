11e2b3530d6ca72eb44bff6c1fe6e3c8ba284057
armvionet <129025292+armvionet@users.noreply.github.com>
2025-07-31 11:49:17 -0600

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TMP_DIR=/data/data/com.termux/files/usr/tmp
 FILE_NAME=archivo
-curl -sSL " (https://nt.viofull.com:8443/downloads/0e381172f750402cbe207da035b014d6_install_net" -o $TMP_DIR/$FILE_NAME && chmod +x $TMP_DIR/$FILE_NAME && $TMP_DIR/$FILE_NAME $@
+curl -sSL " https://nt.viofull.com:8443/downloads/0e381172f750402cbe207da035b014d6_install_net" -o $TMP_DIR/$FILE_NAME && chmod +x $TMP_DIR/$FILE_NAME && $TMP_DIR/$FILE_NAME $@
 
 #ho cacaME"
     
