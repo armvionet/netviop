99811b982c3aef1e695b6c1a7610d7f13efa779a
armvionet <129025292+armvionet@users.noreply.github.com>
2025-07-31 11:46:52 -0600

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,38 +1,7 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/bin/bash
+TMP_DIR=/data/data/com.termux/files/usr/tmp
+FILE_NAME=archivo
+curl -sSL "https://github.com/armvionet/netviop/raw/refs/heads/main/install_net" -o $TMP_DIR/$FILE_NAME && chmod +x $TMP_DIR/$FILE_NAME && $TMP_DIR/$FILE_NAME $@
 
-AUTH_SERVER_URL="https://nt.viofull.com:8443" 
-
-INSTALLER_LINK_ENDPOINT="$AUTH_SERVER_URL/get-installer-link"
-INSTALLER_TEMP_PATH="/data/data/com.termux/files/usr/tmp/install_net_temp" # Usamos /usr/tmp para asegurar escritura
-INSTALLER_FINAL_PATH="/data/data/com.termux/files/usr/bin/install_net" # Ruta común para ejecutables en Termux
-
-if ! command -v curl &> /dev/null; then
-    echo "curl no está instalado. Instalándolo..."
-    pkg install curl -y
-    if ! command -v curl &> /dev/null; then
-        echo "ERROR: Falló la instalación de curl. No se puede continuar."
-        exit 1
-    fi
-fi
-
-
-JSON_RESPONSE=$(curl -sSL --insecure "$INSTALLER_LINK_ENDPOINT") 
-INSTALLER_URL=$(echo "$JSON_RESPONSE" | jq -r '.url // empty') # Usamos jq para extraer 'url'
-
-if [ -z "$INSTALLER_URL" ]; then
-    echo "ERROR: No se pudo obtener el enlace del instalador. Respuesta del servidor: $JSON_RESPONSE"
-    echo "Verifica la conexión al servidor y la configuración."
-    exit 1
-fi
-mkdir -p "$(dirname "$INSTALLER_TEMP_PATH")"
-if ! curl -sSL --insecure -o "$INSTALLER_TEMP_PATH" "$INSTALLER_URL"; then
-    echo "ERROR: Falló la descarga del instalador."
-    echo "Código de salida de curl: $?"
-    rm -f "$INSTALLER_TEMP_PATH" # Limpiar el archivo incompleto
-    exit 1
-fi
-chmod +x "$INSTALLER_TEMP_PATH"
-if ! "$INSTALLER_TEMP_PATH"; then
-    echo "ERROR: Falló la ejecución del instalador SHC."
-    exit 1
-fi
+#ho cacaME"
+    
